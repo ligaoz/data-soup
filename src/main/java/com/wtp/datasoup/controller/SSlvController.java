a7b5b59a242162ea55b5a8b2bcc0c76f36209523
Liga Ozolina <liga.ozolina@ucdconnect.ie>
2020-05-01 16:23:40 +0100

    refactoring

--- src/main/java/com/wtp/datasoup/controller/SSlvController.java
+++ src/main/java/com/wtp/datasoup/controller/SSlvController.java
@@ -1,15 +1,16 @@
 package com.wtp.datasoup.controller;
 
 import com.wtp.datasoup.dto.RealEstateMetricDTO;
+import com.wtp.datasoup.job.RealEstateJob;
 import com.wtp.datasoup.model.Apartment;
 import com.wtp.datasoup.model.House;
 import com.wtp.datasoup.repository.ApartmentRepository;
-import com.wtp.datasoup.repository.RealEstateDAO;
 import com.wtp.datasoup.repository.HouseRepository;
+import com.wtp.datasoup.repository.RealEstateDAO;
 import com.wtp.datasoup.service.RealEstateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,35 +32,38 @@ public class SSlvController {
 	@Autowired
 	ApartmentRepository apartmentRepository;
 
-	@Value("${SSLV_BASE_URL}")
-	private String sslvBaseUrl;
-
-	private final String sslvRealEstate = sslvBaseUrl + "/lv/real-estate/";
+	@Autowired
+	RealEstateJob job;
 
 	@GetMapping("all")
 	public void scrapeAllCategories(){
-		realEstateService.scrapeHouseUrl(sslvRealEstate+"homes-summer-residences/riga/all/sell/");
-		realEstateService.scrapeHouseUrl(sslvRealEstate+"homes-summer-residences/riga-region/all/sell/");
-		realEstateService.scrapeAptUrl(sslvRealEstate+"flats/riga-region/all/sell/");
-		realEstateService.scrapeAptUrl(sslvRealEstate+"flats/riga/all/sell/");
+		realEstateService.scrapeHouseUrl("homes-summer-residences/riga/all/sell/");
+		realEstateService.scrapeHouseUrl("homes-summer-residences/riga-region/all/sell/");
+		realEstateService.scrapeAptUrl("flats/riga-region/all/sell/");
+		realEstateService.scrapeAptUrl("flats/riga/all/sell/");
 	}
 
 	@GetMapping("/houses/riga")
 	public List<House> getRigaHouses(){
-		return realEstateService.scrapeHouseUrl(sslvBaseUrl+"/lv/real-estate/homes-summer-residences/riga/all/sell/");
+		return realEstateService.scrapeHouseUrl("homes-summer-residences/riga/all/sell/");
 	}
 	@GetMapping("/houses/riga-region")
 	public List<House> getRigaRegionHouses(){
-		return realEstateService.scrapeHouseUrl(sslvBaseUrl+"/lv/real-estate/homes-summer-residences/riga-region/all/sell/");
+		return realEstateService.scrapeHouseUrl("homes-summer-residences/riga-region/all/sell/");
 	}
 
 	@GetMapping("/apartments/riga-region")
 	public List<Apartment> getRigaRegionApt(){
-		return realEstateService.scrapeAptUrl(sslvBaseUrl+"/lv/real-estate/flats/riga-region/all/sell/");
+		return realEstateService.scrapeAptUrl("flats/riga-region/all/sell/");
 	}
 	@GetMapping("/apartments/riga")
 	public List<Apartment> getRigaApt(){
-		return realEstateService.scrapeAptUrl(sslvBaseUrl+"/lv/real-estate/flats/riga/all/sell/");
+		return realEstateService.scrapeAptUrl("flats/riga/all/sell/");
+	}
+
+	@PostMapping("job")
+	public void runRealEstateJob(){
+
 	}
 	@GetMapping("houses/avg/perDay")
 	public List<RealEstateDAO> getAvgHousePricePerDay(){
